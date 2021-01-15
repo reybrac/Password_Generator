538765569d6849783564d49a912667edd8c1f47e
Reynaldo Bracamonte <reybrac@yahoo.com>
2021-01-14 16:58:00 -0800

    Additional changes to JS trying a new solution to password generator

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// prompt to ask about using numbers
+var useNumbers = confirm('Do you want to use numbers?');
+console.log(useNumbers);
+
+// prompt to ask about using lower case
+var useLowerCase = confirm('Do you want to use lower case?');
+console.log(useLowerCase);
+
+// prompt to ask about using Upper case
+var useUpperCase = confirm('Do you want to use Upper case?');
+console.log(useUpperCase);
+
+// prompt to ask about using Upper case
+var useSpecialChar = confirm('Do you want to use special characters?');
+console.log(useSpecialChar);
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
 
@@ -10,7 +25,16 @@ var randomFunc = {
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
+    var hasLower = (useLowerCase = true);
+    var hasUpper = (useUpperCase = true);
+    var hasNumber = (useNumbers = true);
+    var hasSymbol = (useSpecialChar = true);
+    var password = generatePassword(
+        hasLower,
+        hasUpper,
+        hasNumber,
+        hasSymbol
+    );
     var passwordText = document.querySelector('#password');
 
     passwordText.value = password;
@@ -23,42 +47,28 @@ function generatePassword(lower, upper, number, symbol) {
     console.log();
 
     let generatedPassword = "";
-
-    // prompt to ask about using numbers
-    var useNumbers = confirm('Do you want to use numbers?');
-    console.log(useNumbers);
-    
-    // prompt to ask about using lower case
-    var useLowerCase = confirm('Do you want to use lower case?');
-    console.log(useLowerCase);
-    
-    // prompt to ask about using Upper case
-    var useUpperCase = confirm('Do you want to use Upper case?');
-    console.log(useUpperCase);
-
-    // prompt to ask about using Upper case
-    var useSpecialChar = confirm('Do you want to use special characters?');
-    console.log(useSpecialChar);
     
     var typesCount = lower + upper + number + symbol;
 
     var typesArr = [{lower} + {upper} + {number} + {symbol}].filter(item => Object.values(item)[0]);
 
-    if(typesCount === 0){
-    return '';
-    }
-        for(let i=0; i < pwlength; i += typesCount){
-            typesArr.forEach(type =>{
-                var funcName = Object.keys(type)[0];
-                console.log('funcName: ' , funcName);
+    console.log('tyesArr: ', typesArr);
 
-                generatedPassword = generatedPassword + randomFunc[funcName]();
-            });
+    if(typesCount === 0){
+        return 'what did you do?';
+    }
+    for(let i=0; i < pwlength; i += typesCount){
+        typesArr.forEach(type =>{
+            var funcName = Object.keys(type)[0];
+            console.log('funcName: ' , funcName);
+
+            generatedPassword = generatedPassword + randomFunc[funcName]();
+        });
     }
       
     var finalPassword = (generatedPassword.slice(0, pwlength));
-    return finalPassword;
-  
+    return "good";
+    console.log(generatedPassword.slice(0, pwlength));
 }
 
 
@@ -74,14 +84,14 @@ function getRandomSymbol() {
   return symbols[Math.floor(Math.random()*symbols.length)];
 }
 function getRandomLower() {
-  var symbols = "abcdefghijklmnopqrstuvwxyz";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
   
-  return symbols[Math.floor(Math.random()*symbols.length)];
+  return lowerCase[Math.floor(Math.random()*lowerCase.length)];
 }
 function getRandomUpper() {
-  var symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   
-  return symbols[Math.floor(Math.random()*symbols.length)];
+  return upperCase[Math.floor(Math.random()*upperCase.length)];
 }
 
 
